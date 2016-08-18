ef2e4d481fec30f0195cdab7ba6ba2960864c4ab
duhlarik <DeeAnn.Uhlarik@gmail.com>
2016-08-18 16:15:07 -0400

    first commit

---
+++ TeachMath.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class TeachMath {
+
+	public static void main(String[] args) {
+
+		Scanner scan1 = new Scanner(System.in);
+
+		Stack<Integer> s = new Stack<Integer>();
+
+		int number;
+		int answer = 0;
+		int x;
+		int y;
+
+		System.out.println("Enter a calculation in Postfix.");
+
+		String input = scan1.nextLine();
+
+		for (int i = 0; i < input.length(); i++) {
+
+			switch (input.charAt(i)) {
+			case '*':
+			case '/':
+			case '+':
+			case '-':
+				x = s.pop();
+				y = s.pop();
+				char operator = input.charAt(i);
+				if (operator == ('*')) {
+					answer = x * y;
+				} else if (operator == ('/')) {
+					answer = x / y;
+				} else if (operator == ('+')) {
+					answer = x + y;
+				} else {
+					answer = x - y;
+				}
+				s.push(answer);
+				break;
+			default:
+				number = Character.getNumericValue(input.charAt(i));
+				s.push(number);
+				break;
+			}
+		}
+		System.out.println(answer);
+
+		scan1.close();
+	}
+
+}
